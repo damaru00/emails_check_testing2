892f90d1e3be651972fc8a4fdf5d5603df793390
damaru00 <jesusjoeymayap@gmail.com>
2016-05-25 13:44:09 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailsCheckTesting2::Application.config.secret_token = 'ded4c6a366241750271a506bd3f2d1eecb0f4a916e36739ba7756228043e2ae48e7535350df76d2d2180ed2328c87c9423c7dcf40c46aa5f571c0b564a13beb8'
